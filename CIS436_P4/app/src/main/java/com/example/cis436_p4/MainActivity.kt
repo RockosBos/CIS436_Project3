b8ed561e13093602be9f38f62276ecbd097cb972
Ahmad Anani <ajanani@umich.edu>
2021-04-26 23:38:12 -0400

    Working onclick

--- CIS436_P4/app/src/main/java/com/example/cis436_p4/MainActivity.kt
+++ CIS436_P4/app/src/main/java/com/example/cis436_p4/MainActivity.kt
@@ -15,7 +15,10 @@ import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import org.json.JSONObject
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,47 +31,6 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
-        // api request variables
-        val queue = Volley.newRequestQueue(this)
 
-        fun getMovie(title: String) {
-            var splitString = title.split(" ").toTypedArray()
-            val newTitle = splitString.joinToString("%20")
-
-            var url = "https://movie-database-imdb-alternative.p.rapidapi.com/?s=&page=1&r=json";
-            url = url.substring(0, 58) + newTitle + url.substring(58, url.length)
-            val keyHeader = "aca0de8833mshd95dc54b87093a2p17c456jsnfd0d32ee10eb";
-            val hostHeader = "movie-database-imdb-alternative.p.rapidapi.com"
-
-            val req = object :
-                StringRequest(Request.Method.GET, url, Response.Listener<String> { response ->
-                    val resp = response.toString()
-                    Log.i("JSONRESPONSE", resp);
-
-                    // Gets json of search
-                    val jsonObj = JSONObject(response)
-                    val jsonArr = jsonObj.getJSONArray("Search")
-                    // index of first movie in list (best match?)
-                    val firstMovie = jsonArr.getJSONObject(0)
-
-                    // all values within the first movie
-                    val movieTitle = firstMovie.getString("Title")
-                    val movieYear = firstMovie.getString("Year")
-                    val movieType = firstMovie.getString("Type")
-                    val moviePoster = firstMovie.getString("Poster")
-
-                }, Response.ErrorListener { Log.d("JSONRESPONSE", "Error!") }) {
-                override fun getHeaders(): MutableMap<String, String> {
-                    val headers = HashMap<String, String>()
-                    headers["x-rapidapi-key"] = keyHeader;
-                    headers["x-rapidapi-host"] = hostHeader;
-                    return headers
-                }
-            }
-
-            queue.add(req)
-        }
-        getMovie("Avengers Endgame")
-        getMovie("I am Legend")
     }
 }
--- CIS436_P4/app/src/main/java/com/example/cis436_p4/ui/main/SearchFragment.kt
+++ CIS436_P4/app/src/main/java/com/example/cis436_p4/ui/main/SearchFragment.kt
@@ -1,11 +1,20 @@
 package com.example.cis436_p4.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.cis436_p4.R
+import org.json.JSONObject
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,12 +37,97 @@ class SearchFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        val inputFragmentView: View = inflater.inflate(R.layout.fragment_search, container, false)
+
+        // api request variables
+        val queue = Volley.newRequestQueue(activity?.applicationContext)
+        var searchBut = view?.findViewById<Button>(R.id.searchButtonSearch)
+        fun getMovie(title: String) {
+            var splitString = title.split(" ").toTypedArray()
+            val newTitle = splitString.joinToString("%20")
+            var url = "https://movie-database-imdb-alternative.p.rapidapi.com/?s=&page=1&r=json";
+            url = url.substring(0, 58) + newTitle + url.substring(58, url.length)
+            val keyHeader = "aca0de8833mshd95dc54b87093a2p17c456jsnfd0d32ee10eb";
+            val hostHeader = "movie-database-imdb-alternative.p.rapidapi.com"
+
+            val req = object :
+                StringRequest(Request.Method.GET, url, Response.Listener<String> { response ->
+                    val resp = response.toString()
+                    Log.d("JSONRESPONSE", resp);
+
+                    // Gets json of search
+                    val jsonObj = JSONObject(response)
+                    val jsonArr = jsonObj.getJSONArray("Search")
+                    // index of first movie in list (best match?)
+                    val firstMovie = jsonArr.getJSONObject(0)
+
+                    // all values within the first movie
+                    val movieTitle = firstMovie.getString("Title")
+                    val movieYear = firstMovie.getString("Year")
+                    val movieType = firstMovie.getString("Type")
+                    val moviePoster = firstMovie.getString("Poster")
+                    // Change widgets
+                    val titleWid = view?.findViewById<TextView>(R.id.titleSearch)
+                    searchBut = view?.findViewById<Button>(R.id.searchButtonSearch)
+                    Log.d("searchbutton", searchBut?.text.toString())
+                    titleWid?.setText(movieTitle)
+
+                }, Response.ErrorListener { Log.d("JSONRESPONSE", "Error!") }) {
+                override fun getHeaders(): MutableMap<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers["x-rapidapi-key"] = keyHeader;
+                    headers["x-rapidapi-host"] = hostHeader;
+                    return headers
+                }
+            }
+
+
+            queue.add(req)
+        }
+
+        fun LOL(title: String) {
+            var splitString = title.split(" ").toTypedArray()
+            val newTitle = splitString.joinToString("%20")
+            var url = "https://movie-database-imdb-alternative.p.rapidapi.com/?s=&page=1&r=json";
+            url = url.substring(0, 58) + newTitle + url.substring(58, url.length)
+            val keyHeader = "aca0de8833mshd95dc54b87093a2p17c456jsnfd0d32ee10eb";
+            val hostHeader = "movie-database-imdb-alternative.p.rapidapi.com"
+
+            val req = object :
+                StringRequest(Request.Method.GET, url, Response.Listener<String> { response ->
+
+                    searchBut = view?.findViewById<Button>(R.id.searchButtonSearch)
+                    searchBut?.setOnClickListener {
+                        val searchText = view?.findViewById<EditText>(R.id.editTextTextPersonNameSearch)
+                        Log.d("ONclickpressed", searchText?.text.toString())
+                        getMovie(searchText?.text.toString())
+                    }
+                    Log.d("searchbutton", searchBut?.text.toString())
+
+                }, Response.ErrorListener { Log.d("JSONRESPONSE", "Error!") }) {
+                override fun getHeaders(): MutableMap<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers["x-rapidapi-key"] = keyHeader;
+                    headers["x-rapidapi-host"] = hostHeader;
+                    return headers
+                }
+            }
+
+
+            queue.add(req)
+        }
+
+        LOL("Avengers Endgame")
+        getMovie("Avengers Endgame")
+        getMovie("I am Legend")
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        return inputFragmentView
     }
 
     companion object {
